816567e72c99d09b3b111fb269bbb13376656325
Geoffrey Gao <geoffreygao1@gmail.com>
2023-04-06 14:10:38 +0900

    Initialized File Structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TreatsTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
